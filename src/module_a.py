bead46d3e00e3d9189128cbaa0d06d009f7d4116
MingLunWu <allen6997535@gmail.com>
2022-03-08 23:39:20 +0800

    [modify] update a example function and its testing case

--- src/module_a.py
+++ src/module_a.py
@@ -1,3 +1,6 @@
+from typing import Type
+
+
 def square(num: int) -> int:
     """範例函式，回傳平方值
 
@@ -8,3 +11,21 @@ def square(num: int) -> int:
         int: 平方後的數值
     """
     return num**2
+
+def concat(str_1: str, str_2: str) -> str:
+    """將兩個字串串接在一起
+
+    Args:
+        str_1 (str): 字串 1
+        str_2 (str): 字串 2
+
+    Raises:
+        TypeError: 當任一參數不為 str 時，拋出 TypeError
+
+    Returns:
+        str: str_1+str_2
+    """
+    if not (isinstance(str_1, str) and isinstance(str_2, str)):
+        raise TypeError("錯誤型態")
+    else:
+        return str_1 + str_2
--- tests/test_module_a.py
+++ tests/test_module_a.py
@@ -1,4 +1,16 @@
-from src.module_a import square
+import pytest
+from src.module_a import square, concat
 
 def test_square():
     assert square(8) == 64
+
+def test_concat_success():
+    str_1 = "Hello! "
+    str_2 = "MingLun!"
+    assert concat(str_1=str_1, str_2=str_2) == "Hello! MingLun!"
+
+def test_concat_failed():
+    str_1 = 555 # Error Type
+    str_2 = 666 # Error Type
+    with pytest.raises(TypeError, match="錯誤型態"):
+        concat(str_1=str_1, str_2=str_2)
