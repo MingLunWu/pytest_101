f9cd2a54009a241130854d6501de9b980f4075f4
MingLun Wu <allen6997535@gmail.com>
2022-03-10 15:18:30 +0800

    [test] update test_module_a.py

--- tests/test_module_a.py
+++ tests/test_module_a.py
@@ -1,14 +1,17 @@
 import pytest
 from src.module_a import square, concat
 
+@pytest.mark.math
 def test_square():
     assert square(8) == 64
 
-def test_concat_success():
+@pytest.mark.string
+def test_concat():
     str_1 = "Hello! "
     str_2 = "MingLun!"
     assert concat(str_1=str_1, str_2=str_2) == "Hello! MingLun!"
 
+@pytest.mark.string
 def test_concat_failed():
     str_1 = 555 # Error Type
     str_2 = 666 # Error Type
